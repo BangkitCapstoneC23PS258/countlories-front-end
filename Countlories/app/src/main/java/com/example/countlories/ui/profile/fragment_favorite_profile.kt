b0223cf974a94c77c21df0c5b77c18e296003d53
Adam Ramadhan J <75119969+natsume1708@users.noreply.github.com>
2023-06-15 02:35:44 +0700

    penambahan fragment di profile activity

--- Countlories/app/src/main/java/com/example/countlories/ui/profile/fragment_favorite_profile.kt
+++ Countlories/app/src/main/java/com/example/countlories/ui/profile/fragment_favorite_profile.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.countlories.R
-
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -30,10 +29,8 @@ class fragment_favorite_profile : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite_profile, container, false)
     }
@@ -48,13 +45,12 @@ class fragment_favorite_profile : Fragment() {
          * @return A new instance of fragment fragment_favorite_profile.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_favorite_profile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                fragment_favorite_profile().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
