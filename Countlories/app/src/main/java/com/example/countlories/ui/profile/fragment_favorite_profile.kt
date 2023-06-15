9ae74d8577579956cda938a4974bad7ae389445f
Adam Ramadhan J <75119969+natsume1708@users.noreply.github.com>
2023-06-15 21:25:26 +0700

    penambahan fragment di profile activity

--- Countlories/app/src/main/java/com/example/countlories/ui/profile/fragment_favorite_profile.kt
+++ Countlories/app/src/main/java/com/example/countlories/ui/profile/fragment_favorite_profile.kt
@@ -1,56 +1,37 @@
-package com.example.countlories.ui.profile
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.countlories.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_favorite_profile.newInstance] factory method to
- * create an instance of this fragment.
- */
-class fragment_favorite_profile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class FavoriteFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var favoriteRecyclerView: RecyclerView
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    private val favoriteList = mutableListOf<String>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_favorite_profile, container, false)
+
+        favoriteRecyclerView = view.findViewById(R.id.favoriteRecyclerView)
+        favoriteAdapter = FavoriteAdapter(favoriteList)
+
+        favoriteRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = favoriteAdapter
         }
-    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_profile, container, false)
-    }
+        // Tambahkan data ke daftar favorite
+        favoriteList.add("Item A")
+        favoriteList.add("Item B")
+        favoriteList.add("Item C")
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_favorite_profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                fragment_favorite_profile().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        return view
     }
 }
